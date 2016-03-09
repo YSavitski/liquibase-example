4528ad814174bb38a70fe13e446d1b5e6df41574
Petr Korobeinikov <pkorobeinikov@gmail.com>
2016-03-10 00:40:28 +0300

    Add non-transactional migration example.

---
+++ db/changelog/db.changelog-v7-alter-type-status-add-in-progress.sql
@@ -0,0 +1,7 @@
+--liquibase formatted sql
+
+--changeset pkorobeinikov:7 runInTransaction:false
+
+alter type status add value 'in progress' after 'confirmed';
+
+-- there is no rollback: drop value is not supported for enums
